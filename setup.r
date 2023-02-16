bc0651e5f20483616322c4adebebce378acb4d80
wangb24 <dummy@wb233.xyz>
2023-02-16 13:36:02 -0800

    add script that install necessary packages

---
+++ setup.r
@@ -0,0 +1,10 @@
+install.packages(
+    c(
+        "languageserver",
+        "httpgd",
+        "RColorBrewer",
+        "paletteer",
+        "ggthemes"
+    ),
+    repos = "https://cran.case.edu/"
+)
